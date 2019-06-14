BUGFIX : Image ratio was not set so image scaling was not right

PodonatorLib.py
@@ -11,19 +11,21 @@ from PIL import Image
 # Change values below for image modification if necessary
 mirror = False
 rotate = True
+image_ratio = 0.4369 # L325/W142
+image_dpi = 148
 
 # Use the calibration.py script to define the values below for each camera
 # Define camera matrix K for camera 1
-K1 = np.array([[728.6058065554909, 0.0, 944.7599470057236], [0.0, 717.4035218893431, 512.8725335118967], [0.0, 0.0, 1.0]])
+K1 = np.array([[805.6337330782276, 0.0, 956.9882395246467], [0.0, 816.8205144586113, 518.6594662094939], [0.0, 0.0, 1.0]])
 
 # Define distortion coefficients d for camera 1
-d1 = np.array([[-0.008902607891725171], [0.09267206754490831], [-0.15736471202694802], [0.08299570424850797]])
+d1 = np.array([[-0.06934545703899442], [0.2681174500565983], [-0.7915276083705534], [0.7514919779408756]])
 
 # Define camera matrix K for camera 2
-K2 = np.array([[728.6058065554909, 0.0, 944.7599470057236], [0.0, 717.4035218893431, 512.8725335118967], [0.0, 0.0, 1.0]])
+K2 = np.array([[805.6337330782276, 0.0, 956.9882395246467], [0.0, 816.8205144586113, 518.6594662094939], [0.0, 0.0, 1.0]])
 
 # Define distortion coefficients d for camera 2
-d2 = np.array([[-0.008902607891725171], [0.09267206754490831], [-0.15736471202694802], [0.08299570424850797]])
+d2 = np.array([[-0.06934545703899442], [0.2681174500565983], [-0.7915276083705534], [0.7514919779408756]])
 
 
 # Gets an image stream from a camera and apply mirroring or 90 degrees CW rotation if necessary
@@ -38,14 +40,14 @@ def get_camera_image(mirror, cam):
 
 # Shows the stream from the cameras and allows for image capture
 # Returns the two captured images (one per camera)
-def show_images(cam1, cam2, rotation):
+def show_images(cam1, cam2, rotate):
     toggle = True
     while toggle:
         img1 = get_camera_image(mirror, cam1)
         img2 = get_camera_image(mirror, cam2)
         if rotate == True:
             #Concatenate the two streams in a single image
-            img = np.concatenate((cv.rotate(img, cv.ROTATE_90_CLOCKWISE), cv.rotate(img, cv.ROTATE_90_COUNTERCLOCKWISE)), axis=1)
+            img = np.concatenate((cv.rotate(img1, cv.ROTATE_90_CLOCKWISE), cv.rotate(img2, cv.ROTATE_90_COUNTERCLOCKWISE)), axis=1)
             #Image resolution to display
             dim = (1080,960)
         else:
@@ -74,19 +76,13 @@ def unwrap_image(img, K, d):
     # Generate look-up tables for remapping the camera image
     mapx, mapy = cv.fisheye.initUndistortRectifyMap(K, d, np.eye(3), K, (w, h), cv.CV_16SC2)
     # Remap the original image to a new image
-    newimg = cv2.remap(img, mapx, mapy, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
-    #Adding padding to left and right to compensate perspective
-    top, bottom = 0, 0
-    delta_w = 2500 - 1920
-    left, right = delta_w//2, delta_w-(delta_w//2)
-    color = [0, 0, 0]
-    newimg = cv2.copyMakeBorder(newimg_img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    newimg = cv.remap(img, mapx, mapy, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
     #Perspective correction
-    pts1 = np.float32([[587,184],[1907,178],[82,1047],[2497,1048]])
-    pts2 = np.float32([[0,0],[2500,0],[0,1080],[2500,1080]])
-    M = cv2.getPerspectiveTransform(pts1,pts2)
-    newimg = cv2.warpPerspective(new_im,M,(2500,1080))
-    newimg = cv2.resize(newimg, (1920, 1080))
+    pts1 = np.float32([[290, 341],[1562, 317],[72, 943],[1834, 907]])
+    pts2 = np.float32([[0, 0],[w, 0],[0, h],[w, h]])
+    M = cv.getPerspectiveTransform(pts1, pts2)
+    newimg = cv.warpPerspective(newimg, M, (w, h))
+    newimg = cv.resize(newimg, (w, int(round(w * image_ratio))))
     return newimg
 
 
@@ -104,14 +100,14 @@ def podonator(output_dir, left_camera_id, right_camera_id):
     #Define image format
     file_ext=".jpg"
     os.chdir(str(Path(output_dir)))
-    cam1 = cv.VideoCapture(left_camera_id)
-    cam2 = cv.VideoCapture(right_camera_id)
+    cam1 = cv.VideoCapture(left_camera_id, cv.CAP_DSHOW)
+    cam2 = cv.VideoCapture(right_camera_id, cv.CAP_DSHOW)
     cam1.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
     cam1.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
     cam2.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
     cam2.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
     #Launch image preview and capture
-    raw_img1, raw_img2 = show_images(cam1, cam2)
+    raw_img1, raw_img2 = show_images(cam1, cam2, rotate)
     #Undistort and correct perspective
     correct_img1 = unwrap_image(raw_img1, K1, d1)
     correct_img2 = unwrap_image(raw_img2, K2, d2)
@@ -124,9 +120,9 @@ def podonator(output_dir, left_camera_id, right_camera_id):
     cv.imwrite(img_name+"_D"+file_ext, correct_img2)
     #Adjust image DPI for printing
     im = Image.open(img_name+"_G"+file_ext)
-    im.save(img_name+"_G"+file_ext, dpi=(152,152))
+    im.save(img_name+"_G"+file_ext, dpi=(image_dpi, image_dpi))
     im = Image.open(img_name+"_D"+file_ext)
-    im.save(img_name+"_D"+file_ext, dpi=(152,152))
+    im.save(img_name+"_D"+file_ext, dpi=(image_dpi, image_dpi))
     #Open the file browser in the output folder
     webbrowser.open(str(Path(output_dir)))
     return