29b263102dfafdcd0266e2f5f1c1c278d3559a4c
mcpzi <mpiuzzi@gmail.com>
2019-06-23 22:49:47 +0200

    Initial stable version

--- PodonatorGUI.py
+++ PodonatorGUI.py
@@ -1,6 +1,7 @@
 from pyforms.basewidget import BaseWidget
 from pyforms.controls   import ControlDir
 from pyforms.controls   import ControlButton
+from pyforms.controls   import ControlNumber
 import sys
 import getopt
 from pathlib import Path
@@ -8,22 +9,20 @@ import PodonatorLib
 
 '''
 usage:
-    PodonatorGUI.py [--left_camera_id] [--right_camera_id]
-
-default values:
-    --left_camera_id  : 0
-    --right_camera_id : 1
+    PodonatorGUI.py
 '''
 
 class PodonatorGUI(BaseWidget):
     def __init__(self, *args, **kwargs):
 
-        super().__init__('Podonator')
-        self.test_camera_flag = True
+        super().__init__('Podonator v1.0')
+        self.test_camera_flag = False
         #Definition of the forms fields
-        self._outputfolder  = ControlDir('Output Folder', default=str(Path().absolute()))
+        self._outputfolder  = ControlDir('Répertoire de sortie', default=str(Path().absolute()))
         self._outputfolder.value = str(Path().absolute())
-        self._runbutton  = ControlButton('Run')
+        self._runbutton  = ControlButton('Acquisition')
+        self._cameraleft = ControlNumber("ID caméra gauche", default=0, decimals=0)
+        self._cameraright = ControlNumber("ID caméra droite", default=1, decimals=0)
 
         #Define the event that will be called when the run button is processed
         self._runbutton.value = self.run_event
@@ -31,6 +30,7 @@ class PodonatorGUI(BaseWidget):
         #Define the organization of the Form Controls
         self._formset = [
             ('','_outputfolder',''),
+            ('','_cameraleft','','_cameraright',''),
             ('','_runbutton','')
         ]
 
@@ -40,19 +40,22 @@ class PodonatorGUI(BaseWidget):
         outputFolder=self._outputfolder.value
 
     def run_event(self):
-        args, optlist = getopt.getopt(sys.argv[1:], '', ['left_camera_id=', 'right_camera_id='])
-        args = dict(args)
-        args.setdefault('--left_camera_id', 0)
-        args.setdefault('--right_camera_id', 1)
-        left_camera_id = int(args.get('--left_camera_id'))
-        right_camera_id = int(args.get('--right_camera_id'))
-        if not PodonatorLib.test_camera(left_camera_id):
-            self.critical("ERROR: No input from left camera, check camera ID", title="Error")
-        if not PodonatorLib.test_camera(right_camera_id):
-            self.critical("ERROR: No input from right camera, check camera ID", title="Error")
-        if PodonatorLib.test_camera(left_camera_id) and PodonatorLib.test_camera(right_camera_id):
-            PodonatorLib.podonator(self._outputfolder.value, left_camera_id, right_camera_id)
-
+        #args, optlist = getopt.getopt(sys.argv[1:], '', ['left_camera_id=', 'right_camera_id='])
+        #args = dict(args)
+        #args.setdefault('--left_camera_id', 0)
+        #args.setdefault('--right_camera_id', 1)
+        #left_camera_id = int(args.get('--left_camera_id'))
+        #right_camera_id = int(args.get('--right_camera_id'))
+        if self.test_camera_flag == False:
+            if not PodonatorLib.test_camera(int(self._cameraleft.value)):
+                self.critical("ERROR: No input from left camera, check camera ID", title="Error")
+            if not PodonatorLib.test_camera(int(self._cameraright.value)):
+                self.critical("ERROR: No input from right camera, check camera ID", title="Error")
+            if PodonatorLib.test_camera(int(self._cameraleft.value)) and PodonatorLib.test_camera(int(self._cameraright.value)):
+                self.test_camera_flag = True
+        if self.test_camera_flag == True:
+            PodonatorLib.podonator(self._outputfolder.value, int(self._cameraleft.value), int(self._cameraright.value))
+        return
 
 if __name__ == '__main__':
 
--- PodonatorLib.py
+++ PodonatorLib.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import datetime
+import collections
 import sys
 import getopt
 from pathlib import Path
@@ -11,8 +12,10 @@ from PIL import Image
 # Change values below for image modification if necessary
 mirror = False
 rotate = True
-image_ratio = 0.4369 # L325/W142
 image_dpi = 148
+image_ratio = 0.4369 # L325/W142
+#Define image format
+file_ext=".jpg"
 
 # Use the calibration.py script to define the values below for each camera
 # Define camera matrix K for camera 1
@@ -22,11 +25,34 @@ K1 = np.array([[805.6337330782276, 0.0, 956.9882395246467], [0.0, 816.8205144586
 d1 = np.array([[-0.06934545703899442], [0.2681174500565983], [-0.7915276083705534], [0.7514919779408756]])
 
 # Define camera matrix K for camera 2
-K2 = np.array([[805.6337330782276, 0.0, 956.9882395246467], [0.0, 816.8205144586113, 518.6594662094939], [0.0, 0.0, 1.0]])
+K2 = np.array([[728.6058065554909, 0.0, 944.7599470057236], [0.0, 717.4035218893431, 512.8725335118967], [0.0, 0.0, 1.0]])
 
 # Define distortion coefficients d for camera 2
-d2 = np.array([[-0.06934545703899442], [0.2681174500565983], [-0.7915276083705534], [0.7514919779408756]])
+d2 = np.array([[-0.008902607891725171], [0.09267206754490831], [-0.15736471202694802], [0.08299570424850797]])
 
+# Reference points for camera 1
+reference_cam1 = np.float32([[290, 341],[1562, 317],[72, 943],[1834, 907]])
+
+# Reference points for camera 2
+reference_cam2 = np.float32([[290, 341],[1562, 317],[72, 943],[1834, 907]])
+
+# Test if the camera is connected
+# Returns False if not, True otherwise
+def test_camera(camera_id):
+    cam = cv.VideoCapture(camera_id)
+    ret_val, image = cam.read()
+    if not ret_val:
+        return False
+    cam.release()
+    return True
+
+# Camera initialization
+def init_camera(camera_id):
+    cam = cv.VideoCapture(camera_id,cv.CAP_DSHOW)
+    cam.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+    cam.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
+    cam.set(cv.CAP_PROP_FPS, 5)
+    return cam
 
 # Gets an image stream from a camera and apply mirroring or 90 degrees CW rotation if necessary
 # Returns the stream with applied corrections
@@ -42,87 +68,89 @@ def get_camera_image(mirror, cam):
 # Returns the two captured images (one per camera)
 def show_images(cam1, cam2, rotate):
     toggle = True
+    gen_output = False
     while toggle:
         img1 = get_camera_image(mirror, cam1)
         img2 = get_camera_image(mirror, cam2)
+        #Undistort
+        u_img1 = undistort_image(img1, K1, d1)
+        u_img2 = undistort_image(img2, K2, d2)
+        #Perspective correction
+        up_img1 = perpective_correction(u_img1, reference_cam1)
+        up_img2 = perpective_correction(u_img2, reference_cam2)
         if rotate == True:
             #Concatenate the two streams in a single image
-            img = np.concatenate((cv.rotate(img1, cv.ROTATE_90_CLOCKWISE), cv.rotate(img2, cv.ROTATE_90_COUNTERCLOCKWISE)), axis=1)
+            img = np.concatenate((cv.rotate(up_img1, cv.ROTATE_90_CLOCKWISE), cv.rotate(up_img2, cv.ROTATE_90_COUNTERCLOCKWISE)), axis=1)
             #Image resolution to display
-            dim = (1080,960)
+            dim = (int(round(960 * image_ratio))*2,960)
         else:
             img = np.concatenate((img1, img2), axis=0)
-            dim = (960,1080)
+            dim = (960,int(round(960 * image_ratio))*2)
         img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
         cv.imshow("Podoscope Preview - Spacebar to acquire or Esc to cancel", img)
         keypress = cv.waitKey(1)
         if keypress%256 == 27:
             #ESC pressed
-            sys.exit("Cancelled")
+            #sys.exit("Cancelled")
+            toggle = False
+            gen_output = False
+            print ("Cancelled")
         elif keypress%256 == 32:
             #SPACE pressed
             toggle = False
+            gen_output = True
             print ("Images acquired")
-    cam1.release()
-    cam2.release()
-    cv.destroyAllWindows()
-    return img1, img2
+    return up_img1, up_img2, gen_output
 
 # Applies correction for lens distortion (K and D parameters obtained through OpenCV calibration for each camera)
 # Returns the image with applied lens distortion correction
-def unwrap_image(img, K, d):
+def undistort_image(img, K, d):
     # Read the image and get its size
     h, w = img.shape[:2]
     # Generate look-up tables for remapping the camera image
     mapx, mapy = cv.fisheye.initUndistortRectifyMap(K, d, np.eye(3), K, (w, h), cv.CV_16SC2)
     # Remap the original image to a new image
     newimg = cv.remap(img, mapx, mapy, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
-    #Perspective correction
-    pts1 = np.float32([[290, 341],[1562, 317],[72, 943],[1834, 907]])
-    pts2 = np.float32([[0, 0],[w, 0],[0, h],[w, h]])
-    M = cv.getPerspectiveTransform(pts1, pts2)
-    newimg = cv.warpPerspective(newimg, M, (w, h))
+    return newimg
+#Perspective correction
+def perpective_correction(img, reference):
+    h, w = img.shape[:2]
+    target = np.float32([[0, 0],[w, 0],[0, h],[w, h]])
+    M = cv.getPerspectiveTransform(reference, target)
+    newimg = cv.warpPerspective(img, M, (w, h))
     newimg = cv.resize(newimg, (w, int(round(w * image_ratio))))
     return newimg
 
-
-# Test if the camera is connected
-# Returns False if not, True otherwise
-def test_camera(camera_id):
-    cam = cv.VideoCapture(camera_id)
-    ret_val, image = cam.read()
-    if not ret_val:
-        return False
-    cam.release()
-    return True
+def output_images(img1, img2, naming_pattern, file_ext, image_dpi):
+    cv.imwrite(naming_pattern+"_G"+file_ext, img1)
+    cv.imwrite(naming_pattern+"_D"+file_ext, img2)
+    #Adjust image DPI for printing
+    im = Image.open(naming_pattern+"_G"+file_ext)
+    im.save(naming_pattern+"_G"+file_ext, dpi=(image_dpi,image_dpi))
+    im = Image.open(naming_pattern+"_D"+file_ext)
+    im.save(naming_pattern+"_D"+file_ext, dpi=(image_dpi,image_dpi))
+    return
 
 def podonator(output_dir, left_camera_id, right_camera_id):
-    #Define image format
-    file_ext=".jpg"
     os.chdir(str(Path(output_dir)))
-    cam1 = cv.VideoCapture(left_camera_id, cv.CAP_DSHOW)
-    cam2 = cv.VideoCapture(right_camera_id, cv.CAP_DSHOW)
-    cam1.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-    cam1.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
-    cam2.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
-    cam2.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
-    #Launch image preview and capture
-    raw_img1, raw_img2 = show_images(cam1, cam2, rotate)
-    #Undistort and correct perspective
-    correct_img1 = unwrap_image(raw_img1, K1, d1)
-    correct_img2 = unwrap_image(raw_img2, K2, d2)
-    if rotate == True:
-        correct_img1 = cv.rotate(correct_img1, cv.ROTATE_90_CLOCKWISE)
-        correct_img2 = cv.rotate(correct_img2, cv.ROTATE_90_COUNTERCLOCKWISE)
-    now=datetime.datetime.now()
-    img_name=now.strftime("%Y-%m-%d-%H%M%S")
-    cv.imwrite(img_name+"_G"+file_ext, correct_img1)
-    cv.imwrite(img_name+"_D"+file_ext, correct_img2)
-    #Adjust image DPI for printing
-    im = Image.open(img_name+"_G"+file_ext)
-    im.save(img_name+"_G"+file_ext, dpi=(image_dpi, image_dpi))
-    im = Image.open(img_name+"_D"+file_ext)
-    im.save(img_name+"_D"+file_ext, dpi=(image_dpi, image_dpi))
-    #Open the file browser in the output folder
-    webbrowser.open(str(Path(output_dir)))
-    return
+    #Initialize cameras
+    cam1 = init_camera(left_camera_id)
+    cam2 = init_camera(right_camera_id)
+    #Launch image preview, capture and correction
+    correct_img1, correct_img2, gen_output = show_images(cam1, cam2, rotate)
+    cam1.release()
+    cam2.release()
+    cv.destroyAllWindows()
+    if gen_output == True:
+        #Rotate images if necessary
+        if rotate == True:
+            correct_img1 = cv.rotate(correct_img1, cv.ROTATE_90_CLOCKWISE)
+            correct_img2 = cv.rotate(correct_img2, cv.ROTATE_90_COUNTERCLOCKWISE)
+        now=datetime.datetime.now()
+        img_name=now.strftime("%Y-%m-%d-%H%M%S")
+        output_images(correct_img1, correct_img2, img_name, file_ext, image_dpi)
+        #Open the file browser in the output folder
+        webbrowser.open(str(Path(output_dir)))
+        return
+    else:
+        return
